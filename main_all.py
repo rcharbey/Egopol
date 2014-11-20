5cd84c8a345fbb6b5bca33fe3a575f4b13bb88b7
Raphaël Charbey <r.charbey@liafa.fr>
2014-11-20 14:06:13 +0100

    changement addresse data

--- main_all.py
+++ main_all.py
@@ -1,10 +1,10 @@
 import sys
 import os
 
-dirname = 'DATA/csa/'
+dirname = 'DATA/csa_2014-09-16/'
 liste_ego = [f for f in os.listdir(dirname) if os.path.isdir(os.path.join(dirname, f))]
 for ego in liste_ego:
-    if not os.path.isdir('./GALLERY/csa/'+ego):
-        sys.argv = ['main.py', 'csa', ego]
+    if not os.path.isdir('./GALLERY/csa_2014-09-16/'+ego):
+        sys.argv = ['main.py', 'csa_2014-09-16', ego]
         execfile("main.py")
         
