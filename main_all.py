9baaf1eac5660fd92128fbc431f9d0abe527e791
Raphaël Charbey <r.charbey@liafa.fr>
2014-12-03 16:08:49 +0100

    pour ajouter options : python main_all -o ...

--- main_all.py
+++ main_all.py
@@ -1,10 +1,19 @@
+import argparse
+parser = argparse.ArgumentParser(description="main")
+parser.add_argument('--options', '-o', nargs='+')
+args = parser.parse_args()
+
 import sys
 import os
 
-dirname = 'DATA/csa_2014-09-16/'
+dirname = 'DATA/csa/'
 liste_ego = [f for f in os.listdir(dirname) if os.path.isdir(os.path.join(dirname, f))]
 for ego in liste_ego:
-    if not os.path.isdir('./GALLERY/csa_2014-09-16/'+ego):
-        sys.argv = ['main.py', 'csa_2014-09-16', ego]
+    if not os.path.isdir('./GALLERY/csa/'+ego):
+        sys.argv = ['main.py', 'csa', ego]
+        if args.options != None:
+            sys.argv.append('-o')
+            for option in args.options:
+                sys.argv.append(option)
         execfile("main.py")
         
