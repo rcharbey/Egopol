4e6d4627ae9f45188cefd1b9d776b133dd460af1
Raphaël Charbey <r.charbey@liafa.fr>
2014-12-03 16:09:53 +0100

    petite erreur de folder

--- main_all.py
+++ main_all.py
@@ -6,11 +6,11 @@ args = parser.parse_args()
 import sys
 import os
 
-dirname = 'DATA/csa/'
+dirname = 'DATA/csa_2014-09-16/'
 liste_ego = [f for f in os.listdir(dirname) if os.path.isdir(os.path.join(dirname, f))]
 for ego in liste_ego:
-    if not os.path.isdir('./GALLERY/csa/'+ego):
-        sys.argv = ['main.py', 'csa', ego]
+    if not os.path.isdir('./GALLERY/csa_2014-09-16/'+ego):
+        sys.argv = ['main.py', 'csa_2014-09-16', ego]
         if args.options != None:
             sys.argv.append('-o')
             for option in args.options:
