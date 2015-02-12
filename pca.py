77d517a73d40921de3f1c51d6a95a65403cc123d
Raphaël Charbey <r.charbey@liafa.fr>
2015-02-12 14:09:01 +0100

    pca fait positions, patterns, 2 et 3D

--- pca.py
+++ pca.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
-import mdp
 import numpy as np
 import os
 import csv
+from matplotlib.mlab import PCA
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import proj3d
 
-def read_enumeration(path):
-    file_to_read = path+'Enumeration/CSV/patterns_friends.csv'
+import argparse
+parser = argparse.ArgumentParser(description="main")
+parser.add_argument('d', help="number of dimensions")
+parser.add_argument('quality', help="patterns or positions")
+args = parser.parse_args()
+
+def read_enumeration(path, quality):
+    file_to_read = path+'Enumeration/CSV/'+quality+'_friends.csv'
     reader = csv.reader(open(file_to_read, 'rb'), delimiter=';')
     result = []
     for line in reader:
@@ -13,20 +22,76 @@ def read_enumeration(path):
         break
     return [float(x) for x in result[0]]
 
-def pick_all_data():
-    result = []
+def pick_all_data(quality):
+    temp = []
+    noms = []
     list_folders = [f for f in os.listdir('GALLERY') if os.path.isdir(os.path.join('GALLERY', f))]
     for folder in list_folders:
         list_ego = [f for f in os.listdir('GALLERY/'+folder) if os.path.isdir(os.path.join('GALLERY/'+folder, f))]
         for ego in list_ego:
-            if os.path.isfile('GALLERY/'+folder+'/'+ego+'/Enumeration/CSV/patterns_friends.csv'):
-                result.append(read_enumeration('GALLERY/'+folder+'/'+ego+'/'))
-    return result
+            if os.path.isfile('GALLERY/'+folder+'/'+ego+'/Enumeration/CSV/'+quality+'_friends.csv'):
+                temp.append(read_enumeration('GALLERY/'+folder+'/'+ego+'/', quality))
+                noms.append((folder, ego))
+    return temp, noms 
 
-data = pick_all_data()
-np_data = np.array(data)
-principal_components = mdp.pca(np_data)
-writer = csv.writer(open('GALLERY/pca.txt', 'wb'), delimiter = ' ')
-for vector in principal_components:
-    writer.writerow(vector)
+def plot_3D(quality):
+    data_temp, noms = pick_all_data(quality)
+    data = np.array(data_temp)
+    try:
+        results = PCA(data)
+    except:
+        raise
+
+    x = []
+    y = []
+    z = []
+    for item in results.Y:
+        x.append(item[0])
+        y.append(item[1])
+        z.append(item[2])
+        
+    pltData = [x,y,z] 
+
+    fig1 = plt.figure() # Make a plotting figure
+    ax = Axes3D(fig1) # use the plotting figure to create a Axis3D object.
+    ax.scatter(pltData[0], pltData[1], pltData[2], 'bo') # make a scatter plot of blue dots from the data
+
+    # make simple, bare axis lines through space:
+    xAxisLine = ((min(pltData[0]), max(pltData[0])), (0, 0), (0,0)) # 2 points make the x-axis line at the data extrema along x-axis 
+    ax.plot(xAxisLine[0], xAxisLine[1], xAxisLine[2], 'r') # make a red line for the x-axis.
+    yAxisLine = ((0, 0), (min(pltData[1]), max(pltData[1])), (0,0)) # 2 points make the y-axis line at the data extrema along y-axis
+    ax.plot(yAxisLine[0], yAxisLine[1], yAxisLine[2], 'r') # make a red line for the y-axis.
+    zAxisLine = ((0, 0), (0,0), (min(pltData[2]), max(pltData[2]))) # 2 points make the z-axis line at the data extrema along z-axis
+    ax.plot(zAxisLine[0], zAxisLine[1], zAxisLine[2], 'r') # make a red line for the z-axis.
+
+    plt.show() # show the plot
+    
+def plot_2D(quality):
+    data_temp, noms = pick_all_data(quality)
+    data = np.array(data_temp)
+    try:
+        results = PCA(data)
+    except:
+        raise
+
+    x = []
+    y = []
+    for item in results.Y:
+        x.append(item[0])
+        y.append(item[1])
+        
+    pltData = [x,y] 
+    fig, ax = plt.subplots()
+    ax.scatter(pltData[0], pltData[1])
+
+    plt.show()
+    
+def main():
+    if args.d == '3D':
+        plot_3D(args.quality)
+    else:
+        plot_2D(args.quality)
+        
+main()
+
                 
