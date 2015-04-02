2e352e71f16e010163fa88bc6b082d6fd76e1df1
Raphaël Charbey <r.charbey@liafa.fr>
2015-04-02 16:20:38 +0200

    dict of mutual -> list

--- main_jsons.py
+++ main_jsons.py
@@ -6,11 +6,14 @@ import read_qualify
 def main(folder, ego, options):
     list_of_friends = read_friends.list_of_friends(folder, ego)
     if options == 'friends' :
-        return read_friends.dict_of_mutual(folder, ego, list_of_friends)
+        return read_friends.list_of_mutual(folder, ego, list_of_friends)
     elif options == 'statuses' :
         return read_statuses.dict_of_mutual_commenters(folder, ego, list_of_friends)
     elif options == 'commenters':
         return read_statuses.dict_of_commenters_per_status(folder, ego)
+    
+def create_correspondence_table(folder, ego):
+    read_friends.create_correspondence_table(folder, ego)
     
 def list_of_friends(folder, ego):
     return read_friends.list_of_friends(folder, ego)
--- read_friends.py
+++ read_friends.py
@@ -4,7 +4,25 @@ import gzip
 import json
 import os
 
-def dict_of_mutual(folder, ego, list_of_friends):
+def create_correspondence_table(folder, ego):
+    path = folder +'/' + ego
+    table_to_write = open('GALLERY/'+path+'/Graphs/correspondence_table', 'w')
+    if os.path.isfile("DATA/"+path+"/friends.jsons"):
+        f = open("DATA/"+path+"/friends.jsons", 'rb')
+    else:
+        gz = "DATA/"+path+"/friends.jsons.gz"
+        f = gzip.open(gz, 'rb')
+    for line in f:
+        friend = json.loads(line)
+        if 'name' in friend:
+            table_to_write.write((friend['name'] + u'\n').encode('utf8'))
+        else:
+            table_to_write.write((friend['id'] + u'\n').encode('utf8'))
+    f.close()    
+    table_to_write.close()
+    
+
+def list_of_mutual(folder, ego, list_of_friends):
     path = folder +'/' + ego
     if os.path.isfile("DATA/"+path+"/friends.jsons"):
         f = open("DATA/"+path+"/friends.jsons", 'rb')
@@ -13,37 +31,24 @@ def dict_of_mutual(folder, ego, list_of_friends):
         f = gzip.open(gz, 'rb')
     result = {}
     
+    n = 0
     for line in f:
         jr = json.loads(line)
-        if 'name' in jr:
-            quality = 'name'
-        else:
-            quality = 'id'
-        result[jr[quality]] = []
+        result.append([])
         if 'mutual' in jr:
             for neighbor in jr['mutual']:
                 if neighbor in list_of_friends:
-                    result[jr[quality]].append(neighbor[quality])
+                    result[n].append(list_of_friends.index(neighbor))
     f.close()
     return result
 
 
 def list_of_friends(folder, ego):
     path = folder +'/' + ego
-    if os.path.isfile("DATA/"+path+"/friends.jsons"):
-        f = open("DATA/"+path+"/friends.jsons", 'rb')
-    else:
-        gz = "DATA/"+path+"/friends.jsons.gz"
-        f = gzip.open(gz, 'rb')
-    
     result = []
+    f = open('GALLERY/'+path+'/correspondence_table', 'r')
     for line in f:
-        friend = json.loads(line)
-        if 'name' in friend:
-            result.append(friend['name'])
-        else:
-            result.append(friend["id"])
-    f.close()    
+        result.append(line)
     return result
 
 def find_friend(folder, ego, id):
