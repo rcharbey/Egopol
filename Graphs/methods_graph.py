ccc3de85c30de09ef2a479e9731e5db78293e97f
Raphaël <r.charbey@gmail.com>
2015-11-12 18:50:24 +0100

    print

--- Graphs/methods_graph.py
+++ Graphs/methods_graph.py
@@ -71,7 +71,7 @@ def gt_coloration(graph):
     for gt in dico:
         for i in range(1,3):
             current_dico = dico[gt][i]
-            print current_dico.values()
+            print np.array(current_dico.values())
             quintiles = np.percentile(np.array(current_dico.values()), np.arange(0, 100, 20))
             for v in graph.vs:
                 value = current_dico.get(v['name'], 0)
