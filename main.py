d9b4a6976ffc181ed5708eb33106b687398938a3
Raphaël <r.charbey@gmail.com>
2015-11-26 19:01:43 +0100

    variable name

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def display(args):
 
 def clusters_per_gt(couple_of_gt, folder, ego):
 
-    dir_path = 'GALLERY/General/Clusters_GT/%s_%s' % (couple_of_gt[0], couple_of_gt[1])
+    dirpath = 'GALLERY/General/Clusters_GT/%s_%s' % (couple_of_gt[0], couple_of_gt[1])
     if not os.path.isdir(dirpath):
         os.mkdir(dirpath)
 
