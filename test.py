16e505bb63329a9198bed5379cc56244494cf447
Raphaël <r.charbey@gmail.com>
2015-11-27 16:45:26 +0100

    modif test.py

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ Created on Fri Nov 27 16:30:51 2015
 @author: raphael
 """
 
-while(True):
-    with open('print_test.txt', 'w') as writer:
+with open('print_test.txt', 'w') as writer:
+    while(True):
         writer.write("2")
