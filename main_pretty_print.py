cee24b4febed51aa789237572d42269431c69557
Raphaël <r.charbey@gmail.com>
2016-06-03 15:43:21 +0200

    pp

--- main_pretty_print.py
+++ main_pretty_print.py
@@ -15,12 +15,10 @@ def gt_pretty_print(folder, ego):
 
 def main():
     list_folders = [f for f in os.listdir('GALLERY') if os.path.isdir(os.path.join('GALLERY', f))]
-    print list_folders
     for folder in list_folders:
         if folder != 'entretiens':
             continue
         list_ego = [f for f in os.listdir('GALLERY/'+folder) if os.path.isdir(os.path.join('GALLERY/'+folder, f))]
-        print list_ego
         for ego in list_ego:
             print 'pretty print : ',
             print ego
@@ -29,13 +27,13 @@ def main():
             for quality in ['friends', 'commenters', 'friends_fc']:
                 pp_patterns.pretty_print(path+'Enumeration/', quality, '../../../../PATTERNS')
                 pp_positions.pretty_print(path+'Enumeration/', quality, '../../../../PATTERNS', list_friends)
-            if not os.path.isdir(path+'Statuses'):
-                continue
-            path = path + 'Statuses/'
-            for quality in ['_induced_friends', '_induced_statuses']:
-                pp_patterns.pretty_print(path+'aggregation_', quality, '../../../PATTERNS')
-            list_statuses = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]
-            for status in list_statuses:
-                for quality in ['_induced_friends', '_induced_statuses']:
-                    pp_patterns.pretty_print(path+status+'/', quality, '../../../../PATTERNS')
+#            if not os.path.isdir(path+'Statuses'):
+#                continue
+#            path = path + 'Statuses/'
+#            for quality in ['_induced_friends', '_induced_statuses']:
+#                pp_patterns.pretty_print(path+'aggregation_', quality, '../../../PATTERNS')
+#            list_statuses = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]
+#            for status in list_statuses:
+#                for quality in ['_induced_friends', '_induced_statuses']:
+#                    pp_patterns.pretty_print(path+status+'/', quality, '../../../../PATTERNS')
 
